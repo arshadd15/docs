e974e1c06dac1f26fd4b8160a5abf09a91154429
Arshad <arshaghnachi234@gmail.com>
2024-12-10 14:40:50 +0530

    first

---
+++ src/components/Card.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import { FaRegFileAlt } from "react-icons/fa";
+import { MdDownloadForOffline } from "react-icons/md";
+import { IoMdClose } from "react-icons/io";
+import { FaRegEdit } from "react-icons/fa";
+import { motion } from "motion/react";
+
+const Card = ({ data, reference }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState(data.desc);
+
+  const EditThis = () => {
+    setIsEditing(true);
+  };
+  const handleTextChange = (e) => {
+    setEditedText(e.target.value);
+  };
+  const handleBlur = () => {
+    setIsEditing(false);
+  };
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      setIsEditing(false);
+    }
+  };
+
+  return (
+    <motion.div
+      drag
+      dragConstraints={reference}
+      whileDrag={{ scale: 1.1 }}
+      dragElastic={0.5}
+      dragTransition={{ bounceStiffness: 100, bounceDamping: 30 }}
+      className="relative flex-shrink-0 w-60 h-72 px-8 py-10 text-white rounded-[30px] bg-zinc-900/80 overflow-hidden backdrop-blur-sm"
+    >
+      <div className="flex justify-between items-center">
+        <FaRegFileAlt />
+        <FaRegEdit onClick={EditThis} />
+      </div>
+      {isEditing ? (
+        <input
+          type="text"
+          value={editedText}
+          onChange={handleTextChange}
+          onBlur={handleBlur}
+          onKeyDown={handleKeyDown}
+          className="mt-5 text-sm leading-tight font-semibold w-full bg-transparent border-b-2 border-white outline-none"
+          autoFocus
+        />
+      ) : (
+        <p className="mt-5 text-sm leading-tight font-semibold">{editedText}</p>
+      )}
+      <div className="footer absolute bottom-0 w-full left-0">
+        <div className="flex justify-between items-center py-3 px-8 mb-3">
+          <h4>{data.filesize}</h4>
+          <span className="text-xl">
+            {data.close ? <IoMdClose /> : <MdDownloadForOffline />}
+          </span>
+        </div>
+        {data.tag.isOpen && (
+          <div
+            className="tag w-full py-4 flex items-center justify-center backdrop-blur-sm"
+            style={{ backgroundColor: `${data.tag.tagColor}` }}
+          >
+            <h3 className="text-md">{data.tag.tagTitle}</h3>
+          </div>
+        )}
+      </div>
+    </motion.div>
+  );
+};
+
+export default Card;
---
+++ src/components/Forground.jsx
@@ -0,0 +1,40 @@
+import React, { useRef } from "react";
+import Card from "./Card";
+
+const Forground = () => {
+  const ref = useRef(null);
+
+  const data = [
+    {
+      desc: "This is the OG card that is created before any other card.",
+      filesize: "0.8 mb",
+      close: true,
+      tag: { isOpen: false, tagTitle: "Download Now", tagColor: "green" },
+    },
+    {
+      desc: "This is the second OG card that is created before any other card.",
+      filesize: "0.8 mb",
+      close: false,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "indianred" },
+    },
+    {
+      desc: "This is the third OG card that is created before any other card.",
+      filesize: "0.8 mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Upload", tagColor: "purple" },
+    },
+  ];
+
+  return (
+    <div
+      ref={ref}
+      className="fixed top-0 left-0 z-[3] w-full h-full flex gap-10 flex-wrap p-5"
+    >
+      {data.map((item, index) => (
+        <Card key={index} data={item} reference={ref} />
+      ))}
+    </div>
+  );
+};
+
+export default Forground;

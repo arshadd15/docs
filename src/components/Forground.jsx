6dd67269b5fe05ade6652ba72384b71887fff2cb
Arshad <arshaghnachi234@gmail.com>
2024-12-18 16:56:00 +0530

    Error resolving

--- src/components/Forground.jsx
+++ src/components/Forground.jsx
@@ -9,7 +9,7 @@ const Forground = () => {
   return (
     <div
       ref={ref}
-      className="fixed top-0 left-0 z-[3] w-full h-full flex flex-wrap p-3 lg:p-5 gap-6 overflow-y-auto overflow-x-hidden scrollbar-transparent"
+      className="fixed top-0 left-0 z-[3] w-full h-full flex flex-wrap p-3 lg:p-5 gap-6 overflow-y-auto overflow-x-hiddenscrollbar-thin scrollbar-thumb scrollbar-thumb-transparent"
     >
       {docs.map((item, index) => (
         <Card key={index} data={item} reference={ref} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,26 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: { plugins: [require("tailwind-scrollbar")] },
+    extend: {},
   },
-  plugins: [
-    function ({ addComponents }) {
-      addComponents({
-        ".scrollbar-transparent::-webkit-scrollbar": {
-          width: "8px",
-          backgroundColor: "rgba(0, 0, 0, 0.1)",
-        },
-        ".scrollbar-transparent::-webkit-scrollbar-track": {
-          backgroundColor: "transparent",
-        },
-        ".scrollbar-transparent::-webkit-scrollbar-thumb": {
-          backgroundColor: "rgba(0, 0, 0, 0.2)",
-          borderRadius: "10px",
-        },
-        ".scrollbar-transparent::-webkit-scrollbar-thumb:hover": {
-          backgroundColor: "rgba(0, 0, 0, 0.7)",
-        },
-      });
-    },
-  ],
+  plugins: [],
 };
